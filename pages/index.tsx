7d6345167c85bf3f901fec6b581f14c823b934b1
Rafael Misato <misatorafael@gmail.com>
2021-04-13 19:51:58 -0300

    feature: initial structure

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>B4YOU</title>
+      </Head>
+
+      <main>
+        <div>Hello World</div>
+      </main>
+    </>
+  );
+}

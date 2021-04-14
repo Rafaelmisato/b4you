6ced6c4bca80e2e96c4fcbf3bee20721d2a25c84
Rafael Misato <misatorafael@gmail.com>
2021-04-14 20:32:10 -0300

    feature: animations on sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,15 +27,33 @@ const Home: React.FC<MenuProps> = () => {
       balance="538,75"
     >
       <Container>
-        {state.menu && (
+        {state.menu === 'home' && (
           <HomeContainer>
             <Video
               src="https://assets.mixkit.co/videos/preview/mixkit-rain-falling-on-the-water-of-a-lake-seen-up-18312-large.mp4"
-              poster={null}
+              poster="/videoThumb.svg"
             />
             <h2>Link para Vendas</h2>
             <InputQr value="https://b4pay.com.br.checkout/p-Y7Qp" />
           </HomeContainer>
+        )}
+
+        {state.menu === 'produtos' && (
+          <section>
+            <h1>Produtos</h1>
+          </section>
+        )}
+
+        {state.menu === 'treinamentos' && (
+          <section>
+            <h1>treinamentos</h1>
+          </section>
+        )}
+
+        {state.menu === 'ranking' && (
+          <section>
+            <h1>ranking</h1>
+          </section>
         )}
       </Container>
     </Layout>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -4,9 +4,22 @@ export const Container = styled.section`
   background: #0f1b35;
   width: calc(100% - 320px);
   padding: 60px 120px;
+
+  > section {
+    animation: zoom 0.2s linear;
+  }
+
+  @keyframes zoom {
+    from {
+      transform: scale(0.8);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
 `
 
-export const HomeContainer = styled.div`
+export const HomeContainer = styled.section`
   h2 {
     margin: 54px 0 40px;
     color: #fff;
